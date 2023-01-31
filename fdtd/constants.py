38e7ce316a90a7e9e86ead8c450ff7045e1530f9
kobari12 <kobari0124@gmail.com>
2023-01-31 17:30:29 +0900

    Update: constants

--- fdtd/constants.py
+++ fdtd/constants.py
@@ -1,5 +1,5 @@
 
 C    = 2.99782458e8            # speed of light[m/s]
 Z0   = 376.7303134617706554679 # impedance of free space
-EPS0 = 1 / (Z0 * C)            # 誘電率
-MU0  = Z0 / C                  # 透磁率
+EPS0 = 1 / (Z0 * C)            # permittivity ε0
+MU0  = Z0 / C                  # permeability μ0
